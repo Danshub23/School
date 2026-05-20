d58d353e18cfd01387c45d6329955db17641ffb0
File <schooldshu@gmail.com>
2026-05-20 09:58:42 +0800

    Update Game.js

--- html/jsg/Game.js
+++ html/jsg/Game.js
@@ -3,7 +3,11 @@ const config = {
     type: Phaser.AUTO,      // 渲染器类型，优先WebGL，失败则自动降级到Canvas
     width: 800,             // 游戏窗口宽度
     height: 600,            // 游戏窗口高度
-    scene: {                // 游戏场景
+    scene: {        
+        function preload ()
+{
+    this.load.image('cyp', 'img/cyp.jepg');
+    }// 
         create: function() {
             // 在画面中央 (400, 300) 添加一段白色的文字
             this.add.text(400, 300, 'Hello Phaser!', {
@@ -11,10 +15,7 @@ const config = {
                 fill: '#fff'
             }).setOrigin(0.5); // 设置文字中心点
         }
-        function preload ()
-{
-    this.load.image('cyp', 'img/cyp.png');
-    }
+        
 };
 
 // 创建并启动游戏实例
