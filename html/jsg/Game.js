35a9fe2845e743df328bef3db3b1b16da7b20b0b
File <schooldshu@gmail.com>
2026-05-22 13:41:49 +0800

    Update Game.js

--- html/jsg/Game.js
+++ html/jsg/Game.js
@@ -4,7 +4,6 @@ const config = {
     height: 1200,
     scene: {
         preload: function() {
-            // 修正图片扩展名（原为 .jepg，现改为常见 .jpeg 或 .jpg）
             this.load.image('cyp', 'img/IMG_0876.jpeg');
             
         },
