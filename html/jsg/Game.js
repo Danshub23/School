6b56b1ad3c2a62b8c5599af9c6f82c645b3ce398
File <schooldshu@gmail.com>
2026-05-22 13:52:24 +0800

    Update Game.js

--- html/jsg/Game.js
+++ html/jsg/Game.js
@@ -1,13 +1,13 @@
 const config = {
     type: Phaser.AUTO,
-    width: 1600,
-    height: 1200,
+    width: 800,
+    height: 600,
     scene: {
         preload: function() {
             this.load.image('cyp', 'img/IMG_0876.jpeg');
         },
         create: function() {
-            let image = this.add.image(800, 600, 'cyp');
+            let image = this.add.image(400, 300, 'cyp');
             image.setScale(0.25);   // 缩小为原来的1/4
         }
     }
