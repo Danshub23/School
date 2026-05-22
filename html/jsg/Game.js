5cc3a142ae8182b8f6fdafc0e94adb0288ca677e
File <schooldshu@gmail.com>
2026-05-22 13:47:52 +0800

    Update Game.js

--- html/jsg/Game.js
+++ html/jsg/Game.js
@@ -1,19 +1,19 @@
 const config = {
-    type: Phaser.AUTO,      
+    type: Phaser.AUTO,
     width: 1600,
     height: 1200,
     scene: {
         preload: preload,
-        create: create,
-
+        create: create
     }
 };
+
+function preload() {
+    this.load.image('cyp', 'img/IMG_0876.jpeg');
+}
+
+function create() {
+    this.add.image(800, 600, 'cyp');
+}
+
 const game = new Phaser.Game(config);
-        preload: function() {
-            this.load.image('cyp', 'img/IMG_0876.jpeg');
-            
-        },
-        create: function() {
-            this.add.image(800, 600, 'cyp');
-            
-        }
