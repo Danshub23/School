9f085add1464b99411b55a1d5fdb4b9e43c48b80
File <schooldshu@gmail.com>
2026-06-06 16:08:19 +0800

    Update check.js

--- code/JS/check.js
+++ code/JS/check.js
@@ -1,11 +1,11 @@
 function check(link) {
         if (document.referrer !== link) {
           document.body.style.display = "none";
-          window.location.replace('https://danshub23.github.io/School/code/test.html');
+          window.location.replace('https://danshub23.github.io/School/less/Chinese/Chinese.html');
         }
       }
 function run() {
-        window.location.replace("https://danshub23.github.io/School/code/test.html");
+        window.location.replace("https://danshub23.github.io/School/less/Chinese/Chinese.html");
       }
 window.addEventListener("pageshow", function(){
         check("https://danshub23.github.io/School/code/test.html")
